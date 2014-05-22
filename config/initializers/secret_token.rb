8ea0e79005299d6c512624b641d730e76eb5c022
ThomasDebek <tom.debek@gmail.com>
2014-05-22 20:29:20 +0200

    Gem devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AppGemy::Application.config.secret_key_base = '308cb89aa05b4be45d0364c1be78f970276acfe6fe30d9acb2a01c12e2dbaa94bb9ea1832de3020402efcf007f91ba3da15cc42cd47c3ca01e32f43cd6e9fab8'
